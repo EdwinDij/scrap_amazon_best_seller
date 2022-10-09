2b392d3824008863fef69937d529be1f1fb05f91
EdwinDij <edwin.d899@gmail.com>
2022-10-09 22:25:22 +0200

    fonction pour chaque boucle pour les produit informatique bestseller

--- main copy.py
+++ main copy.py
@@ -1,4 +1,4 @@
-from urllib import request
 import requests
-from bs4 import BeautifulSoup
-import soupsieve
+from bs4 import BeautifulStoneSoup
+from techProduct import tech_product_name, tech_product_rank, tech_product_stars
+
--- techProduct.py
+++ techProduct.py
@@ -1,15 +1,32 @@
-from urllib import request
 import requests
 from bs4 import BeautifulSoup
-import soupsieve
 url = "https://www.amazon.fr/gp/bestsellers/computers"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
 tech_product_rank = []
-tech_product_link = []
 tech_product_name = []
-
+tech_product_stars = []
 rank = soup.find_all('span', class_='zg-bdg-text')
+name = soup.find_all('div', class_='_cDEzb_p13n-sc-css-line-clamp-3_g3dy1')
+stars = soup.find_all('span', class_='a-icon-alt')
 
-print(rank)
+def get_name ():
+    for product_name in name:
+        tech_product_name.append(product_name.string)
+    return tech_product_name
+get_name()
+
+def get_rank (): 
+    for product_rank in rank:
+        tech_product_rank.append(product_rank.string)
+    return tech_product_rank
+get_rank()
+
+def get_stars ():
+    for product_stars in stars:
+        tech_product_stars.append(product_stars.string)
+    return tech_product_stars
+get_stars()
+
+print(tech_product_name)
